1451331e67d596a3abbc03f33f9b7613df31ce03
137213582@QQ.com <137213582@QQ.com@Sivan.lan>
2020-07-15 16:03:08 +0800

    nice dong

--- src/main/java/com/emsystem/dao/UserLoginDao.java
+++ src/main/java/com/emsystem/dao/UserLoginDao.java
@@ -3,22 +3,12 @@ package com.emsystem.dao;
 import org.apache.ibatis.annotations.ResultType;
 import org.apache.ibatis.annotations.Select;
 
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.ResultType;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
 import com.emsystem.pojo.UserLogin;
 
-
-
-public class UserLoginDao {
+public interface UserLoginDao {
 	//登录 用户名或密码都可以使用
 	@Select("select * from t_user where flag=2 and (username=#{name} or email=#{email})")
 	@ResultType(UserLogin.class)
-	public
-	UserLogin select(String name) {
-		return null;
-	}
+	//public
+	UserLogin select(String name);
 }
