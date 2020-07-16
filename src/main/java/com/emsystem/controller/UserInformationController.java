4100ff29d90484c60625bf8dc08da7f91e01e90b
137213582@QQ.com <137213582@QQ.com@Sivan.lan>
2020-07-16 22:28:02 +0800

    zyq

--- src/main/java/com/emsystem/controller/UserInformationController.java
+++ src/main/java/com/emsystem/controller/UserInformationController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.emsystem.controller;
 
 
@@ -18,33 +17,13 @@ import com.emsystem.pojo.UserInformation;
 import com.emsystem.service.UserInformationService;
 
 @Controller
-=======
-package com.emsystem.controller;
-
-
-import java.io.IOException;
-
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.multipart.MultipartFile;
-
-import com.emsystem.pojo.UserInformation;
-import com.emsystem.service.UserInformationService;
-
-@Controller
->>>>>>> branch 'ceshi' of https://github.com/dingyadong/EmployeeManagementSystem.git
 	public class UserInformationController {
 	@Autowired
 	private UserInformationService userI;
 
 	// 查询模糊
-<<<<<<< HEAD
 	@RequestMapping("selectByName")
-	public String selectByName(String name, Model model) {
+	public String selectByName1(String name, Model model) {
 		model.addAttribute("UserList", userI.queryByName(name));
 		return "page/table";
 		}
@@ -69,7 +48,7 @@ import com.emsystem.service.UserInformationService;
 	// 员工新增
 	@RequestMapping(value = "/adduser")
     @ResponseBody
-    public String save(MultipartFile file, UserInformation user, HttpServletRequest request)
+    public String save1(MultipartFile file, UserInformation user, HttpServletRequest request)
 			throws IllegalStateException, IOException {
 		if (userI.save(user)) {
 			System.out.print("成功");
@@ -80,32 +59,6 @@ import com.emsystem.service.UserInformationService;
 		}
 	}
 }
-=======
-	@RequestMapping("selectByName")
-	public String selectByName(String name, Model model) {
-		model.addAttribute("UserList", userI.queryByName(name));
-		return "page/table";
-		}
-	
-	// 查看商品列表
-	@RequestMapping("getallusers")
-	public String goodslist(Model model) {
-		model.addAttribute("goodsList", userI.queryAll());
-		return "admin/showGoods";
-	}
-	
-	// 员工新增
-	@RequestMapping("adduser")
-	public String save(MultipartFile file, UserInformation user, HttpServletRequest request)
-			throws IllegalStateException, IOException {
-		if (userI.save(user)) {
-			return "index";
-		} else {
-			request.setAttribute("msg", "添加失败，重新再来");
-			return "index";
-		}
-	}
-	
-	
-}
->>>>>>> branch 'ceshi' of https://github.com/dingyadong/EmployeeManagementSystem.git
+
+
+
